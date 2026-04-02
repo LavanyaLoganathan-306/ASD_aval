61dc52a96e5fe1ee9cc15ff7671446be1b623740
lavanya loganathan <lavanyaloganathan306@gmail.com>
2026-04-02 13:24:22 +0530

    reverse

---
+++ reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class reverse {
+    public static int Reverse(int n){
+       int reverse = 0;
+       int digit =0;
+       while(n>0){
+          digit = n%10;
+          reverse = reverse*10 + digit;
+          n = n/10;
+       }
+       return reverse;
+    }
+
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan .nextInt();
+        int result = Reverse(n);
+        System.out.println(result);
+    }
+}
